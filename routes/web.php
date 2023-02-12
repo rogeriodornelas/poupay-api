8950b497045695408365639f335ba674fa0f8a62
ericlempe <ericlempe1994@gmail.com>
2023-02-12 15:35:02 -0300

    removido rota welcome

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +12,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
