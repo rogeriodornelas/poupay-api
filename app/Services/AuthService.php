b45edac3dd979743e9570649df51977c7bd72526
ericlempe <ericlempe1994@gmail.com>
2023-03-19 14:21:29 -0300

    ajustes na criacao de grupo ao logar via social

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\User;
 use Exception;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use StdClass;
 
@@ -12,6 +13,13 @@ class AuthService
     public function __construct()
     {
         $this->jwtService = new JwtService();
+    }
+
+    public function create($payload)
+    {
+        //todo
+
+        # vincular o primeiro grupo
     }
 
     public function login($payload)
@@ -39,6 +47,29 @@ class AuthService
     {
         $payload = new StdClass();
         $payload->user_id = $user_id;
+        $payload->group_id = (new GroupService())->list()->first()->id;
         return $this->jwtService->createToken($payload);
+    }
+
+    public function loginFromProvider($provider, $payload)
+    {
+        return DB::transaction(function () use ($provider, $payload) {
+            $user = (new User())->updateOrCreate(
+                ['provider_id' => $payload->getId()],
+                [
+                    'name' => $payload->getName(),
+                    'email' => $payload->getEmail(),
+                    'provider_name' => $provider,
+                    'provider_id' => $payload->getId(),
+                ]
+            );
+
+            if ($user->has("groups")->get()->count() == 0) {
+                $group = (new GroupService())->create(['name' => "Minhas Contas"]);
+                $user->groups()->attach($group->id);
+            }
+
+            return $this->getToken($user->id);
+        });
     }
 }
