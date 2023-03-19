a32c90786cc7a349341c404323daeacd2aa2e9b5
ericlempe <ericlempe1994@gmail.com>
2023-03-19 14:15:07 -0300

    add grupo de rotas de pagamento

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,4 +11,10 @@ Route::post('login/{provider}/callback', [AuthController::class, 'handleProvider
 
 Route::middleware(['auth.api'])->group(function () {
     Route::get('/user', [AuthController::class, 'getUser']);
+
+    Route::prefix('payment')->group(function () {
+        Route::get('/', [PaymentController::class, 'list']);
+        Route::post('/', [PaymentController::class, 'create']);
+        Route::put('/{id}', [PaymentController::class, 'update'])->where('invoice_id', '[0-9]+');
+    });
 });
